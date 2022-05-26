f22cef846e4b6a813c5e5c372e7ffaef4bff7e99
Aditya <adityasingh220598@gmail.com>
2022-05-26 21:47:42 +0530

    Adding user login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,28 +1,98 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [loading, setIsLoading] = useState(false);
+  const [idToken, setIdToken] = useState("");
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredEmail = emailRef.current.value;
+    const enteredPassword = passwordRef.current.value;
+    // Optional: Add validation
+    if (isLogin) {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCL4yDwp5GiPNEgN3qZzWGinqbl4Gu0q1s",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "applcation/json",
+          },
+        }
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            response.json().then((error) => {
+              alert(error?.error?.message);
+            });
+          }
+        })
+        .then((data) => {
+          console.log(data);
+        });
+    } else {
+      setIsLoading(true);
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCL4yDwp5GiPNEgN3qZzWGinqbl4Gu0q1s",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+        .then((response) => {
+          setIsLoading(false);
+          if (response.ok) {
+            return response.json();
+          } else {
+            response.json().then((error) => {
+              let errorMessage = "Authentication failed";
+              errorMessage = error?.error?.message;
+              alert(errorMessage);
+            });
+          }
+        })
+        .then((data) => {
+          console.log(data);
+        });
+    }
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <input ref={emailRef} type="email" id="email" required />
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <input ref={passwordRef} type="password" id="password" required />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          {!loading && <button>{isLogin ? "Login" : "Create Account"}</button>}
+          {loading && <p>Sending Request</p>}
           <button
             type="button"
             className={classes.toggle}
