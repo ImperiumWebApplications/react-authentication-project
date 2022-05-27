9dcc6ab4f536a0d19386c31f81719a1348b31081
Aditya <adityasingh220598@gmail.com>
2022-05-27 09:26:35 +0530

    Adding reset password functionality

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,50 @@
-import classes from './ProfileForm.module.css';
+import { useContext, useRef } from "react";
+import { AuthContext } from "../../store/auth-context";
+import classes from "./ProfileForm.module.css";
 
-const ProfileForm = () => {
+const ProfileForm = (props) => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredNewPassword = newPasswordInputRef.current.value;
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCL4yDwp5GiPNEgN3qZzWGinqbl4Gu0q1s",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return response.json().then((error) => {
+          alert(error?.error?.message);
+        });
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input ref={newPasswordInputRef} type="password" id="new-password" />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
