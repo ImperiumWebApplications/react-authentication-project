c57662049d350174bb9a9675455c53d63ca062ed
Aditya <adityasingh220598@gmail.com>
2022-05-28 10:34:18 +0530

    Adding auto logout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -8,14 +8,30 @@ export const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = (expirationTime) => {
+  // Calculate remaining time for the token
+  // expirationTime needs to converted to date object
+  const expirationDate = new Date(expirationTime);
+  const currentDate = new Date();
+  const remainingTime = expirationDate.getTime() - currentDate.getTime();
+  return remainingTime;
+};
+
 const AuthContextProvider = (props) => {
   const intiialToken = localStorage.getItem("idToken");
   const [token, setToken] = useState(intiialToken);
   const userIsLoggedIn = !!token;
-  const loginHandler = (token) => {
+
+  const loginHandler = (token, expirationTime) => {
     setToken(token);
     localStorage.setItem("idToken", token);
+    const remainingTime = calculateRemainingTime(expirationTime);
+    // Remove the idToken from localStorage after the remaining time
+    setTimeout(() => {
+      logoutHandler();
+    }, remainingTime);
   };
+
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem("idToken");
